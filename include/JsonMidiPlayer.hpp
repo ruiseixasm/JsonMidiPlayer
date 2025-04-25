9bed5cf9b4ce5fcd757bbb010850bc8e6b52946d
Rui Monteiro <ruiseixas@yahoo.com>
2025-04-25 12:49:58 +0100

    Changed version number to 5.0.2

--- include/JsonMidiPlayer.hpp
+++ include/JsonMidiPlayer.hpp
@@ -44,7 +44,7 @@ https://github.com/ruiseixasm/JsonMidiPlayer
 // #define DEBUGGING true
 #define FILE_TYPE "Json Midi Player"
 #define FILE_URL  "https://github.com/ruiseixasm/JsonMidiPlayer"
-#define VERSION   "5.0.1"
+#define VERSION   "5.0.2"
 #define DRAG_DURATION_MS (1000.0/((120/60)*24))
 
 

1aabcad7e193f1df663544dea4ce05e87a62f696
Rui <ruiseixas@yahoo.com>
2026-09-11 10:21:18 +0100

    Added more constructors

--- include/JsonMidiPlayer.hpp
+++ include/JsonMidiPlayer.hpp
@@ -195,6 +195,16 @@ public:
             priority(priority)
         { }
 
+    // Pin constructor from time_ms and position_beats (num, den)
+    MidiPin(double time_milliseconds, uint32_t num, uint32_t den, MidiDevice* midi_device,
+        const std::vector<unsigned char>& json_midi_message, const unsigned char priority = 0xFF)
+            : time_ms(time_milliseconds),
+            _position_beat(Beat::fromFraction(num, den)),
+            midi_device(midi_device),
+            midi_message(json_midi_message),    // Directly initialize midi_message
+            priority(priority)
+        { }
+
     // Pin copy constructor
     MidiPin(const MidiPin& other)
         : time_ms(other.time_ms),                     // Copy the time_ms

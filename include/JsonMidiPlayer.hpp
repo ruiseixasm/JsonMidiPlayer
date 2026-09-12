e1fd08dd713d5b17d030a7a46db5d6e821683bec
Rui <ruiseixas@yahoo.com>
2026-09-12 12:19:09 +0100

    Safer code

--- include/JsonMidiPlayer.hpp
+++ include/JsonMidiPlayer.hpp
@@ -480,17 +480,21 @@ public:
 		uint32_t left_ticks = left.getPositionTicks();
 		uint32_t right_ticks = right.getPositionTicks();
 		if (left_ticks <= right_ticks) {
-			int16_t left_bpm_10 = static_cast<int16_t>(left.getBPM_10());
-			int16_t right_bpm_10 = static_cast<int16_t>(right.getBPM_10());
+			int16_t left_bpm_10 = left.getBPM_10();
+			int16_t right_bpm_10 = right.getBPM_10();
 			if (ticks <= left_ticks) {
 				return (double)ticks * 625 / left_bpm_10;
 			} else if (ticks >= right_ticks) {
-				return (double)(ticks - right_ticks) * 625 / left_bpm_10;
-			} else {	// Trapezoid (implicit exclusion of `left_ticks == right_ticks` based on the above consitions)
-				uint32_t delta_ticks = right_ticks - left_ticks;
-				int16_t delta_bpm_10 = right_bpm_10 - left_bpm_10;
-				int16_t interpolated_bpm_10 = delta_bpm_10 / delta_ticks * (ticks - left_ticks);
-				return (double)(ticks - left_ticks) * 625 / interpolated_bpm_10 / 2;
+				return (double)(ticks - right_ticks) * 625 / right_bpm_10;
+			} else if (left_bpm_10 == right_bpm_10) {
+				return (double)(ticks - left_ticks) * 625 / left_bpm_10;
+			} else {	// Trapezoid (exclusion of `left_ticks == right_ticks` and `left_bpm_10 == right_bpm_10`)
+				uint32_t delta_ticks = right_ticks - left_ticks;	// Never 0 (excluded above)
+				int16_t delta_bpm_10 = right_bpm_10 - left_bpm_10;	// Never 0 (excluded above)
+				double ticks_bpm_10 = (double)delta_bpm_10 / delta_ticks * (ticks - left_ticks);
+				double base_time_ms = (double)ticks * 625 / left_bpm_10;
+				double slope_time_ms = (double)(ticks - left_ticks) * 625 / (ticks_bpm_10 - left_bpm_10) / 2;
+				return base_time_ms + slope_time_ms;
 			}
 		}
 		return 0.0;
